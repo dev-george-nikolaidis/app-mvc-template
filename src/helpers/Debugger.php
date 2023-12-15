20013c0d2c6d1ca2089a23566b5a354a18c5cd33
George Sparrow <dev.georgios.nikolaidis@gmail.com>
2023-12-15 18:00:37 +0100

    add Debugger

---
+++ src/helpers/Debugger.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\helpers;
+
+class Debugger
+{
+    public static function log(string $message, string $file = 'debug.log')
+    {
+        $timestamp = date("[Y-m-d H:s:s]");
+        $logMessage = "$timestamp $message\n";
+
+        // Log to a file
+        file_put_contents($file, $logMessage, FILE_APPEND);
+
+        // Output to the console
+        echo $logMessage;
+    }
+
+
+
+    public static function dump(mixed $var)
+    {
+        echo "<pre>";
+        var_dump($var);
+        echo "</pre>";
+    }
+
+}
